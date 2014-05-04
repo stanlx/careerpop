585dda620d822415341c50fd287ba8e299d7c509
Stan Melamed <stanmelamed@gmail.com>
2014-05-04 13:41:22 +0000

    added validation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :user_survey_sections
 	has_many :answers
+	validates :fname, presence: true, length: {maximum: 25}
+  validates :lname, presence: true, length: {maximum: 30}
+	VALID_EMAIL_ADDRESS = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_ADDRESS }
 end
