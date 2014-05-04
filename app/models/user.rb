1c98b49814ce586bfd9a0d0efac8d48167b68edb
Stan Melamed <stanmelamed@gmail.com>
2014-05-04 13:45:31 +0000

    added validation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
 	validates :fname, presence: true, length: {maximum: 25}
   validates :lname, presence: true, length: {maximum: 30}
 	VALID_EMAIL_ADDRESS = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, format: { with: VALID_EMAIL_ADDRESS }
+	validates :email, presence: true, format: { with: VALID_EMAIL_ADDRESS }, 
+																						uniqueness: {case_sensitive: false}
 end
